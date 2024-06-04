2cd545eccac9e0e22b4db3aab2d16766ba5b0afe
alaa.elsayed <alaa.elsayed00@outlook.com>
2024-06-04 14:45:39 +0300

    ProductController

---
+++ Production_Managment/Production.Core/Interface/Service/IProductPlanning.cs
@@ -0,0 +1,13 @@
+﻿using Production.Core.DataTransferObject;
+
+namespace Production.Core.Interface.Service
+{
+    public interface IProductPlanning
+    {
+        Task<ProductPlanningDto> CreateAsync(ProductPlanningDto plan);
+        Task<ProductPlanningDto> UpdateAsync(ProductPlanningDto plan);
+        Task DeleteAsync(int id);
+        Task<ProductPlanningDto> GetByIdAsync(int id);
+        Task<IEnumerable<ProductPlanningDto>> GetAllProducts();
+    }
+}
---
+++ Production_Managment/Production.Services/ProductPlanningService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Production.Core.DataTransferObject;
+using Production.Core.Interface.Repositories;
+using Production.Core.Interface.Service;
+
+namespace Production.Services
+{
+    public class ProductPlanningService : IProductPlanning
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductPlanningService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<ProductPlanningDto> CreateAsync(ProductPlanningDto plan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductPlanningDto>> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductPlanningDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductPlanningDto> UpdateAsync(ProductPlanningDto plan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
